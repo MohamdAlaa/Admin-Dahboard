a05b5391ecfc4912c87e4b549e94e13881b58cd0
Mohamed Alaa <mohamedelmenyawe606@gmail.com>
2023-09-27 10:41:12 +0300

    finished TopDealUser

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,14 @@
+import TopBox from "../../component/topBox/TopBox";
 import "./home.scss"
+
 
 
 const Home = () => {
   return (
     <div className="home">
-      <div className="box box1">Box1</div>
+      <div className="box box1">
+        <TopBox />
+      </div>
       <div className="box box2">Box2</div>
       <div className="box box3">Box3</div>
       <div className="box box4">Box4</div>
@@ -13,8 +17,7 @@ const Home = () => {
       <div className="box box6">Box6</div>
       <div className="box box7">Box7</div>
       <div className="box box8">Box8</div>
-      <div className="box box1">Box1</div>
-      <div className="box box10">Box10</div>
+      <div className="box box9">Box9</div>
     </div>
   )
 };5
